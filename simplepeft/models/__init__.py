8f686096c6179a1d4cd81474acef98b0ae901e18
flozi00 <flozi00.fz@gmail.com>
2023-06-30 16:54:53 +0200

    undo llama context

--- simplepeft/models/__init__.py
+++ simplepeft/models/__init__.py
@@ -85,25 +85,6 @@ def get_model(
                     None,
                     [],
                 )
-            elif model_type == "llama":
-                old_init = (
-                    transformers.models.llama.modeling_llama.LlamaRotaryEmbedding.__init__
-                )
-
-                def ntk_scaled_init(
-                    self, dim, max_position_embeddings=2048, base=10000, device=None
-                ):
-                    # The method is just these three lines
-                    max_position_embeddings = 16384
-                    a = 8  # Alpha value
-                    base = base * a ** (dim / (dim - 2))  # Base change formula
-
-                    old_init(self, dim, max_position_embeddings, base, device)
-
-                transformers.models.llama.modeling_llama.LlamaRotaryEmbedding.__init__ = (
-                    ntk_scaled_init
-                )
-
             elif model_type in ["wav2vec2", "mctct"]:
                 conf.update(
                     {
