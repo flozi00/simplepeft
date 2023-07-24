821b85d30734f37bf0eb891e4f528cd23ccc1ebb
flozi00 <flozi00.fz@gmail.com>
2023-07-24 10:12:27 +0200

    ignore outliers losses

--- simplepeft/train/train.py
+++ simplepeft/train/train.py
@@ -8,54 +8,68 @@ from torch.optim.lr_scheduler import ExponentialLR
 
 warnings.simplefilter("ignore")
 
-TEMP_LIMIT = 72
-ACCUMULATION_STEPS = 2
+TEMP_LIMIT = 70
 
 
 def start_training(
     model, processor, dloader, PEFT_MODEL, LR: float, model_conf: dict, batch_size: int
 ):
-    accelerator = Accelerator(
-        gradient_accumulation_steps=ACCUMULATION_STEPS, log_with="wandb"
-    )
+    accelerator = Accelerator(log_with="wandb")
     device = accelerator.device
     accelerator.init_trackers("huggingface")
 
     if model_conf["is8bit"]:
-        from bitsandbytes.optim import PagedAdamW8bit
+        from bitsandbytes.optim import PagedAdam
 
-        optim = PagedAdamW8bit(model.parameters(), lr=LR)
+        optim = PagedAdam(model.parameters(), lr=LR)
     else:
         optim = Lion(model.parameters(), lr=LR)
 
-    scheduler = ExponentialLR(optim, gamma=0.98)
+    scheduler = ExponentialLR(optim, gamma=0.95)
     model, optim, dloader, scheduler = accelerator.prepare(
         model, optim, dloader, scheduler
     )
 
     model.train()
-    index = 0
+    index = 1
+    losses = []
     for data in (pbar := tqdm(dloader)):
-        with accelerator.accumulate(model):
-            index += 1
-            if index % 100 == 0:
-                model.save_pretrained(
-                    PEFT_MODEL,
-                    save_function=accelerator.save,
-                    state_dict=accelerator.get_state_dict(model),
-                )
-            optim.zero_grad()
-            output = model(return_dict=True, **data)
-            loss = output.loss
-            accelerator.backward(loss)
+        if index % 100 == 0:
+            model.save_pretrained(
+                PEFT_MODEL,
+                save_function=accelerator.save,
+                state_dict=accelerator.get_state_dict(model),
+            )
+
+        optim.zero_grad()
+        output = model(return_dict=True, **data)
+        loss = output.loss
+        accelerator.backward(loss)
+
+        if len(losses) < 2:
+            losses.append(loss)
+        elif len(losses) > 4:
+            losses.pop(0)
+
+        avg_loss = sum(losses) / len(losses)
+
+        pbar.set_description(
+            f"Loss: {loss}, Average_loss: {avg_loss}, Step trained: {index-1}",
+            refresh=True,
+        )
+
+        if loss < 2 * avg_loss:
+            losses.append(loss)
             optim.step()
             scheduler.step()
-            accelerator.log({"training_loss": loss}, step=index - 1)
-            pbar.set_description(f"Loss: {loss}", refresh=True)
+            accelerator.log(
+                {"training_loss": loss, "average_loss": avg_loss}, step=index - 1
+            )
+            index += 1
 
-            gpus = GPUtil.getGPUs()
-            for gpu_num in range(len(gpus)):
-                gpu = gpus[gpu_num]
-                if gpu.temperature >= TEMP_LIMIT:
-                    faktor = int(gpu.temperature) - TEMP_LIMIT
-                    time.sleep(faktor * 5)
+        gpus = GPUtil.getGPUs()
+        for gpu_num in range(len(gpus)):
+            gpu = gpus[gpu_num]
+            if gpu.temperature >= TEMP_LIMIT:
+                faktor = int(gpu.temperature) - TEMP_LIMIT
+                time.sleep(faktor * 5)
